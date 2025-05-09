1248d72116dda5115d027f6499c8ce5ed2ae3b5c
Alej01902 <alejo29cl@gmail.com>
2025-05-09 00:48:04 -0500

    Agregue linea a Layout y codigo en Create

---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public CreateModel(SupermarketContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid||_context.categories==null||Category == null)
+            {
+                return Page();
+            }
+            _context.categories.Add(Category);
+            await _context.SaveChangesAsync();
+            return RedirectToPage(" ./index");
+        }
+    }
+}
