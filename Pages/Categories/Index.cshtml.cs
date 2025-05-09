3cabaedb5751fcdf540f63fc26dc0a4d0e1dc031
Alej01902 <alejo29cl@gmail.com>
2025-05-08 23:36:56 -0500

    css HTML

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly SupermarketContext _context;
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> categories { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.categories != null)
+            {
+                categories = await _context.categories.ToListAsync();
+            }
+        }
+    }
+}
