863b823b2e0f96f068a29dbd0747d991eb807482
Alej01902 <alejo29cl@gmail.com>
2025-05-10 14:10:31 -0500

    Creando tablas migraciones

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,7 +6,7 @@ namespace SupermarketWEB.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        [Column(TypeName = "Decimal(6,2")]
+        [Column(TypeName = "Decimal(6,2)")]
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
